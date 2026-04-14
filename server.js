952d784381f5ee12896173ca504294cd429d0fed
sigmaDestroyer <thomas.ag.schmitt@gmail.com>
2026-04-14 14:12:57 +0200

    ajout du serveur

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 3000;
+
+app.use('/style', express.static(path.join(__dirname, 'style')));
+app.use('/script', express.static(path.join(__dirname, 'script')));
+app.use('/lib', express.static(path.join(__dirname, 'lib')));
+app.use('/page', express.static(path.join(__dirname, 'page')));
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.redirect('/home.html');
+});
+
+app.get('/:pageName', (req, res) => {
+    const pageName = req.params.pageName;
+    const filePath = path.join(__dirname, 'page', `${pageName}`);
+    
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            res.status(404).send('Page non trouvée');
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`serveur lancé sur http://localhost:${PORT}/`);
+});
